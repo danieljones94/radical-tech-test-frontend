225440e1d562b43d37704aebcfa89167297d8e6e
Daniel Jones <danielmjones1010@gmail.com>
2021-05-16 18:24:57 +0100

    live routing

--- src/App.js
+++ src/App.js
@@ -5,34 +5,32 @@ import Header from "./components/header/Header";
 import "./App.css";
 import frontpageimage from "./assets/images/frontpageimage.png";
 import welcomeMessage from "./assets/images/welcomemessage.png";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <div className="row">
-          <div className="col-md-8">
-            <img
-              alt="Welcome message"
-              src={welcomeMessage}
-              className="welcomeMessage"
-            />
-            <img
-              alt="Landing page theme"
-              src={frontpageimage}
-              className="landingPageImage"
-            />
-          </div>
-          <div className="col-md-4">
-            <Route path="/" exact component={LoginPage}></Route>
-            <Route path="/signup" exact component={SignUpPage}></Route>
-            <Route path="/home" exact component={LandingPage}></Route>
-          </div>
+    <div className="app">
+      <Header />
+      <div className="row">
+        <div className="col-md-8">
+          <img
+            alt="Welcome message"
+            src={welcomeMessage}
+            className="welcomeMessage"
+          />
+          <img
+            alt="Landing page theme"
+            src={frontpageimage}
+            className="landingPageImage"
+          />
+        </div>
+        <div className="col-md-4">
+          <Route path="/" exact component={LoginPage}></Route>
+          <Route path="/signup" exact component={SignUpPage}></Route>
+          <Route path="/home" exact component={LandingPage}></Route>
         </div>
       </div>
-    </Router>
+    </div>
   );
 }
 
